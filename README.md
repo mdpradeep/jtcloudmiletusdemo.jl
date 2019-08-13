8033a2dd1f21cb31f9b7968a516a8132f8ecc00c
Pradeep Mudlapur <pradeep@juliacomputing.com>
2019-08-13 22:28:21 +0530

    Test

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # jtclouddemo.jl
 JuliaTeam Cloud Demo
+
+This is an example of creating applications that could be run on JuliaTeam Cloud
---
+++ bin/mail.jl
@@ -0,0 +1,10 @@
+@everywhere using Miletus, DelimitedFiles, Dates, Distributed
+@everywhere run(`wget -q https://externalshare.blob.core.windows.net/demos/miletus/portfolio.csv.1M`)
+@everywhere p = readdlm("portfolio.csv.1M", ',', header=true)[1];
+
+@time @distributed (vcat) for i = 1:1000000
+    m = CRRModel(today(), Date(p[i,3]), 250, p[i, 1], 0.1, 0.05, 0.15)
+    ap = AmericanPut(Date(p[i, 3]), SingleStock(), p[i, 2])
+    value(m, ap)
+end
+
