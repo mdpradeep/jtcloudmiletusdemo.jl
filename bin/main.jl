e8fee8bfe0ea7396d3b9fe45c6726891791915a7
mdpradeep <13034211+mdpradeep@users.noreply.github.com>
2019-12-24 20:07:29 +0530

    test

--- bin/main.jl
+++ bin/main.jl
@@ -1,12 +1,17 @@
 ## @everywhere Pkg.add("Miletus")
 @info "Running jcloudmiletusdemo !"
+ENV["DRIVER_SCRIPT"] = @__FILE__
+@info "Driver script ", ENV["DRIVER_SCRIPT"]
+
 @everywhere using Miletus, DelimitedFiles, Dates, Distributed
 @everywhere run(`wget -q https://externalshare.blob.core.windows.net/demos/miletus/portfolio.csv.1M`)
 @everywhere p = readdlm("portfolio.csv.1M", ',', header=true)[1];
 
-@time @distributed (vcat) for i = 1:1000000
+@time @distributed (vcat) for i = 1:100
     m = CRRModel(today(), Date(p[i,3]), 250, p[i, 1], 0.1, 0.05, 0.15)
     ap = AmericanPut(Date(p[i, 3]), SingleStock(), p[i, 2])
     value(m, ap)
 end
-
+write("abc.txt", "Test")
+ENV["RESULTS_FILE_TO_UPLOAD"] = "abc.txt"
+@info "Results file ", ENV["RESULTS_FILE_TO_UPLOAD"]
