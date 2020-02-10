49f17f0452bca92d3ec0979194af2e6306a23577
mdpradeep <13034211+mdpradeep@users.noreply.github.com>
2020-02-10 16:17:14 +0530

    updated

--- bin/main.jl
+++ bin/main.jl
@@ -8,7 +8,7 @@ ENV["DRIVER_SCRIPT"] = @__FILE__
 @everywhere run(`wget -q https://externalshare.blob.core.windows.net/demos/miletus/portfolio.csv.1M`)
 @everywhere p = readdlm("portfolio.csv.1M", ',', header=true)[1];
 
-@time @distributed (vcat) for i = 1:100
+@time @distributed (vcat) for i = 1:10 # default 100
     m = CRRModel(today(), Date(p[i,3]), 250, p[i, 1], 0.1, 0.05, 0.15)
     ap = AmericanPut(Date(p[i, 3]), SingleStock(), p[i, 2])
     value(m, ap)
